5dc45ff30056c875f9b61649e7c7f5acfa1e69dc
albiszon <kju@wemif.net>
2014-09-24 10:20:11 +0200

    klasa Board

--- main.cpp
+++ main.cpp
@@ -1,20 +1,52 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
-
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <ifaddrs.h>
 #include <netinet/in.h>
+
 #include "SimpleUDP.h"
 
 using namespace std;
 
+class Board
+{
+    protected:
+        SimpleUDP *udp;
+    public:
+        Board()
+        {
+            udp = new SimpleUDP("192.168.207.255", 1234);
+        }
+
+        virtual ~Board() { delete udp; }
+
+        virtual void led(uint8_t r, uint8_t g, uint8_t b)
+        {
+            if(r > 100) r = 100;
+            if(g > 100) g = 100;
+            if(b > 100) b = 100;
+            string command = "rgb " + to_string(r) + " " + to_string(g) + " " + to_string(b);
+            udp->send(command);
+        }
+};
+
+
 
 int main()
 {
+    Board board;
+    for(;;)
+    {
+        board.led(rand()%101, rand()%101, rand()%101);
+        usleep(300000);
+    }
+
+
+/*
     SimpleUDP udp("192.168.207.255", 1234);
     udp.send("rgb 100 0 0"); usleep(300000);
     udp.send("rgb 0 100 0"); usleep(300000);
@@ -23,6 +55,8 @@ int main()
     cout << "waiting for a message from network..." << endl;
     cout << udp.receive() << endl;
     cout << endl;
+
+*/
 
 /*
     UDP *udp = new UDP();
